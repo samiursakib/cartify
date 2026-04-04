f1eb06513cf7de37cf695d246f2737c3a9618831
samiursakib <rafisamiur@gmail.com>
2026-04-05 03:46:33 +0600

    pyproject.toml config setup for starting application

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to Cartify!"}
+
+def start():
+    uvicorn.run(app, host="0.0.0.0", port=8080)
